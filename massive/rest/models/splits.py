43fe84761950a9e537f526c26ec83eb633cc3a56
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2025-12-29 02:10:18 -0800

    Added floats, splits, dividends, and risk factors endpoints and models (#980)

--- massive/rest/models/splits.py
+++ massive/rest/models/splits.py
@@ -14,3 +14,26 @@ class Split:
     @staticmethod
     def from_dict(d):
         return Split(**d)
+
+
+@modelclass
+class StockSplit:
+    adjustment_type: Optional[str] = None
+    execution_date: Optional[str] = None
+    historical_adjustment_factor: Optional[float] = None
+    id: Optional[str] = None
+    split_from: Optional[float] = None
+    split_to: Optional[float] = None
+    ticker: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d):
+        return StockSplit(
+            adjustment_type=d.get("adjustment_type"),
+            execution_date=d.get("execution_date"),
+            historical_adjustment_factor=d.get("historical_adjustment_factor"),
+            id=d.get("id"),
+            split_from=d.get("split_from"),
+            split_to=d.get("split_to"),
+            ticker=d.get("ticker"),
+        )
